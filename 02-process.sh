93ac5e0df75143ceb6d44253b35eb6ea0421db8b
DennaGherlyn <denna@mailbox.org>
2025-05-23 13:49:47 +0200

    Add comment to clarify required audio encoding for Dreame robots

--- 02-process.sh
+++ 02-process.sh
@@ -6,4 +6,5 @@
 inputdir=output/speech
 outputdir=output/result
 
+# Required audio encoding for Dreame robots
 ffmpeg-normalize $inputdir/*.wav --normalization-type peak --target-level 0 -of $outputdir -c:a libvorbis -b:a 100k -ar 16000 -ofmt ogg -ext ogg
