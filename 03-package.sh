8287b7d327c88f18d775eda270f57b80288c23c9
DennaGherlyn <denna@mailbox.org>
2025-05-24 09:46:13 +0200

    Enhance output clarity by adding spacing and descriptive message for MD5 hash generation in packaging script

--- 03-package.sh
+++ 03-package.sh
@@ -7,8 +7,10 @@ tar czv * > ../voice_pack.tar.gz
 cd ../..
 
 ls -l output/voice_pack.tar.gz
+echo ""
+echo "Generating MD5 hash for the voice pack"
 md5sum output/voice_pack.tar.gz
-
+echo ""
 echo "Host the file with python3 -m http.server"
 echo "Visit the Valetudo web interface"
 echo "Select Hamburger menu / Robot settings / Misc Settings"
